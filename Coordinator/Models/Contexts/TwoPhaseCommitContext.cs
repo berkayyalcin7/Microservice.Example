ba4fca97c1da70f40a1484cde72b74120404c087
Berkay <berkayyalcin7@icloud.com>
2024-08-05 22:35:50 +0300

    Coordinator
    Coordinator for Strong Consistency

---
+++ Coordinator/Models/Contexts/TwoPhaseCommitContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Coordinator.Models.Contexts
+{
+    public class TwoPhaseCommitContext:DbContext
+    {
+        public TwoPhaseCommitContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Node> Nodes { get; set; }
+
+        public DbSet<NodeState> NodeState { get; set; }
+    }
+}
---
+++ Coordinator/Program.cs
@@ -0,0 +1,54 @@
+using Coordinator.Models.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TwoPhaseCommitContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient("OrderAPI", client => client.BaseAddress = new Uri("https://localhost:7247"));
+builder.Services.AddHttpClient("StockAPI", client => client.BaseAddress = new Uri("https://localhost:7013"));
+builder.Services.AddHttpClient("PaymentAPI", client => client.BaseAddress = new Uri("https://localhost:7048"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
